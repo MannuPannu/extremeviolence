cfd92198f61f6d3d170809c0aebdbc9dc0ad329b
Magnus Stenqvist <magnus.p.stenqvist@gmail.com>
2015-11-29 21:39:28 +0100

    My first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # extremeviolence
+# extreme violence
---
+++ game.js
@@ -0,0 +1,15 @@
+var requestAnimFrame = (function(){
+  return window.requestAnimationFrame       ||
+         window.webkitRequestAnimationFrame ||
+         window.mozRequestAnimationFrame    ||
+         window.oRequestAnimationFrame      ||
+         window.msRequestAnimationFrame     ||
+         function(callback, element){
+           window.setTimeout(callback, 1000 / 60);
+         };
+})();
+
+
+window.onload = function() {
+	console.log("Game start");
+};
